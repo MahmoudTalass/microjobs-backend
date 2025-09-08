ce753f3ffc3d1331a4d0a323def6cafd6f7c2d57
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2025-09-08 14:33:12 -0400

    init spring boot app

---
+++ src/main/java/com/microjobs/microjobs/MicrojobsApplication.java
@@ -0,0 +1,13 @@
+package com.microjobs.microjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrojobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrojobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microjobs/microjobs/MicrojobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microjobs.microjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrojobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
